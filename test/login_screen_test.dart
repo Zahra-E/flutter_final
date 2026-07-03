66aaba16462a23315fe230980aa40933bf110c1e
Zahra-E <zahraelgaoud@gmail.com>
2026-07-03 18:10:36 +0200

    last commit

---
+++ test/login_screen_test.dart
@@ -0,0 +1,11 @@
+﻿import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pokemon_final/screens/login_screen.dart';
+
+void main() {
+  testWidgets('shows a Google sign-in button on the login screen', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: LoginScreen()));
+
+    expect(find.text('Continue with Google'), findsOneWidget);
+  });
+}
